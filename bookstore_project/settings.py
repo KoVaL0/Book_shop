223ca1f8d98ab17f5e2ba805ae62d04f0255e516
KoVaL <goks223300@gmail.com>
2019-10-29 00:15:46 +0300

    Create app 'Pages' and make tests

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',   # add app pages
+    'users.apps.UsersConfig',   # add app users_form
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],   # new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- users/forms.py
+++ users/forms.py
@@ -7,11 +7,11 @@ class CustomUserCreationForm(UserChangeForm):
 
 	class Meta(UserCreationForm):
 		model = get_user_model()
-		fields = ('email', 'username',)
+		fields = ('email', 'username',)		#string for creation new form
 
 
 class CustomUserChangeForm(UserChangeForm):
 
 	class Meta(UserChangeForm):
 		model = get_user_model()
-		fields = ('email', 'username',)
+		fields = ('email', 'username',)		#string for change new form
